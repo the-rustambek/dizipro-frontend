512657416c0326de3a2396fab9fbfaf47fa2cb67
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-01 16:55:57 +0500

    add children

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   const isTrue = true;
   return (
     <>
+    <Header>
+      <h1>Salom sayt</h1>
+    </Header>
     <div className="App">
       {isTrue && <h2>Salom dunyo</h2>}
     </div>
     <p>Test</p>
     </>
   );
+}
+
+
+function Header({children}){
+  console.log(children);
+  return <header>{children}</header>;
 }
 
 export default App;
