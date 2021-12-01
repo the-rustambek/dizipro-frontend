105ab148601f2c5210ac94a8ccf0939b110b9150
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-01 17:27:21 +0500

    add count useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,17 @@ import {useState} from "react";
 
 function App() {
   
-  const [active,setActive] = useState(false);
+  const [count,setCount] = useState(1);
   
-  // let active = false;
-
-  // let setActive = (qiymat) =>{
-  //   active = qiymat
-  // }
 
   const handlePress = () =>{
-    setActive(!active);
+    setCount(count + 1);
   }
  
   return (
     <>
     <Header />
-    <div className="App">{active &&  <h2>Salom dunyo</h2>}</div>
+    <div className="App"><h2>{count}</h2></div>
     
     <button onClick={handlePress}>Meni bos</button>
     </>
