1dcb9a78023a6b0a82df875b50b3e131b3ed9834
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-01 17:15:46 +0500

    add header.jsx navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.scss';
+import Header from "./components/Header";
 
 function App() {
   const isTrue = true;
   return (
     <>
-    <Header color="red">
-      <h1>Salom sayt</h1>
-    </Header>
-    <div className="App">
-      {isTrue && <h2>Salom dunyo</h2>}
-    </div>
-    <p>Test</p>
+    <Header />
+    <div className="App">{isTrue &&  <h2>Salom dunyo</h2>}</div>
+    
     </>
   );
 }
 
 
-function Header({children,color}){
-  
-  return <header className={color === "red" ? "red": null}>{children}</header>;
-}
+
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header(){
+    return (
+        <header>
+            <h1>Dizipro.uz</h1>
+        
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/contact">COntact</a>
+            </li>
+        </ul>
+        
+        </header>
+    )
+}
