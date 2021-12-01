1a2d1c389b9fa8ebe3dc234e8ef3e38ca96db329
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-01 16:44:49 +0500

    add && bittalik shart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const isTrue = false;
+  const isTrue = true;
   return (
     <>
     <div className="App">
-      {isTrue ? <h2>Salom dunyo</h2> : <h2>Xayr dunyo</h2>}
+      {isTrue && <h2>Salom dunyo</h2>}
     </div>
     <p>Test</p>
     </>
