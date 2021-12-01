481b003fb55fbf198615382ec96efdbc962307e1
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-01 17:57:33 +0500

    add useEffect bu sahifa ishga tushhgan paytda ishga tushadigan hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,19 @@ import logo from './logo.svg';
 import './App.scss';
 import Header from "./components/Header";
 import {useState} from "react";
+import {useEffect} from "react";
 
 function App() {
   
-  const [todos,setTodos] = useState([]);
-  
-
-  const handleSubmit  =   (event) =>{
-    event.preventDefault();
-
-    setTodos([...todos,event.target[0].value]);
-
-    event.target.reset();
-
+  const test = () =>{
+    console.log("test");
   };
+
+  useEffect(test);
  
   return (
     <>
-    <ul>
-      {todos.map((todos,i) =>{
-        return <li key = {i} >{todos}</li>;
-      })}
-    </ul>
-    
-  <form onSubmit={handleSubmit}>
-    <input type="text" />
-    <button >Meni bos</button>
-  </form>
+    <h1>test</h1>
     </>
   );
 }
