d9277cea50bfff4eca5d899a99531981e4e7da2b
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-07 14:23:52 +0500

    aadd

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
+
 import './App.scss';
 import Header from "./components/Header";
 import {useState} from "react";
-import {useEffect} from "react";
+
+
 
 function App() {
   
-  const test = () =>{
-    let x = setInterval(() =>{
-      setRaqam(raqam + 1);
-    },1000);
-  };
-  
-const [raqam,setRaqam] = useState(0);
+const [state, setState]
+ = useState(false);
 
-  useEffect(test);
- 
-  return (
-    <>
-    <h1>{raqam}</h1>
-    </>
-  );
+
+ return (
+   <>
+  {state && <Header />}
+  <button onClick={() => setState(!state)} > Meni bos 
+  </button>
+   </>
+ )
+
+
+
 }
 
 export default App;
