d696b3c2a50378b0b723a7467b238360fc3b5063
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-08 17:42:28 +0500

    add hello world react . react beginner finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
 import './App.scss';
 
-import {useContext, useState} from "react";
-import { BrowserRouter,Redirect, Route } from 'react-router-dom';
-import Home from "./pages/Home/Home"
-import About from "./pages/About/About"
-import Header from './components/Header';
-import ColorContext, { ColorProvider } from './contexts/ColorContext';
-import AuthContext, { AuthProvider } from './contexts/AuthContext';
-import Login from './pages/Login/Login';
-import Profile from './pages/Profile/Profile';
-
 
 function App() {
-
-const [color, setColor] = useState("white");
-
-
-return(
-<h1>Hello world</h1>
-)
-
-
-
+return <h1>Hello world</h1>
 }
 
 export default App;
 
-
-function ProvidedRoute(...props){
-  const {token,setToken} = useContext(AuthContext);
-  if(!token){
-    return (
-      <Redirect to={{
-        pathname: "/",
-      }} />
-    )
-  }
-  return <Route {...props[0]} />
-}
