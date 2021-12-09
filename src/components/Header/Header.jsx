ad47d2cbf371c4216e0754e44a25e63b9f681e39
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-09 14:41:13 +0500

    add img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import "./Header.scss";
+import Logo from "../../assets/Images/dizipro.png";
 
 export default function Header(){
-    return <header className="header">
-   <div className="container">
-  
-   </div>
-    </header>
+return ( 
+    <header className="header">
+        <div className="container">
+            <img src={Logo} alt="" />
+        </div>
+    </header>   
+    );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,9 @@
+import Header from "../../components/Header/Header";
 import "./Login.scss";
 
 export default function Login(){
     return <div className="login">
+        <Header />
     </div>
 
 }
