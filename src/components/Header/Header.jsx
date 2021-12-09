b9ba1b72cb81379fac4419c96d3ec84528497d18
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-09 15:54:27 +0500

    add beeImage and beginnerr template

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,11 @@ export default function Header(){
 return ( 
     <header className="header">
         <div className="container">
-            <img src={Logo} alt="" />
+            <img src={Logo} alt="Logo" />
+            <select name="languages" id="languages" className="header__language-select-element">
+                <option value="ru">RU</option>
+            </select>
+        
         </div>
     </header>   
     );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,21 @@
 import Header from "../../components/Header/Header";
 import "./Login.scss";
-
+import BeeImage from "../../assets/Images/asal_ari.png"
 export default function Login(){
     return <div className="login">
         <Header />
+        <section className="login__main-section">
+          <div className="container">
+              
+            <img src={BeeImage}className="login__be-image" alt="" />
+            <form className="login__form">
+                <h2 className="login__title">Login</h2>
+            </form>
+            
+           
+          </div>
+              
+        </section>
     </div>
 
 }
