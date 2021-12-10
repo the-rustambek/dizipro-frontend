a6e849502c5293830f7c111ed822a51d469b1221
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-12-10 11:07:31 +0500

    add fixed 32

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,8 @@
+import "./Input.scss";
+
+export default function Input (props){
+    return  <label>
+    <input className="input" {...props} />
+    </label>
+    
+}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,24 @@
 import Header from "../../components/Header/Header";
 import "./Login.scss";
-import BeeImage from "../../assets/Images/asal_ari.png"
+import BeeImage from "../../assets/Images/asal_ari.png";
+import Input from "../../components/Input/Input";
+
 export default function Login(){
-    return <div className="login">
-        <Header />
-        <section className="login__main-section">
-         
-            <img src={BeeImage}className="login__be-image" alt="" />
-         
-         <div className="login__form-wrapper">
-         <form className="login__form">
+return <div className="login">
+    <Header />
+    <section className="login__main-section">
+
+        <img src={BeeImage}className="login__be-image" alt="" />
+
+        <div className="login__form-wrapper">
+            <form className="login__form">
                 <h2 className="login__title">Login</h2>
-                
+                    <div className="login__form__inputs">
+                        <Input type="email" placeholder="email" required />
+                    </div>
             </form>
-         </div>
-      
-              
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
 
 }
